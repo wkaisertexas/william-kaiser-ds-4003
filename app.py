b3010e40c876fedac78fb34279b04d2b8d95b763
William Kaiser <wkaisertexas@gmail.com>
2024-04-04 10:55:55 -0400

    fix: updated plot to add regression functionality

--- app.py
+++ app.py
@@ -19,7 +19,7 @@
 # 
 # For more information about data provenance, collection, formatting, and cleaning, please see [Sprint 2: Data](./sprints/sprint2-data.ipynb).
 
-# In[53]:
+# In[167]:
 
 
 # Generic imports
@@ -35,7 +35,7 @@ from typing import List, Dict, TypedDict
 import os
 
 
-# In[54]:
+# In[168]:
 
 
 # Loading the data
@@ -44,7 +44,7 @@ df['course_level'] = pd.to_numeric(df['course_number']).apply(lambda x: x // 100
 df
 
 
-# In[55]:
+# In[169]:
 
 
 # Printing the columns
@@ -55,7 +55,7 @@ print("\n".join(df.columns.to_list()))
 # 
 # The goal of this section is for a specified course to be selected and then a radar plot to be generated based on the reviews for that course.
 
-# In[56]:
+# In[170]:
 
 
 REVIEW_COMPONENTS = {
@@ -109,7 +109,7 @@ def get_course_summary_ratings(pneumonic: str, number: int) -> pd.DataFrame:
 get_course_summary_ratings(*course_components(COURSE))
 
 
-# In[57]:
+# In[171]:
 
 
 # Creating a spider plot for the course
@@ -165,7 +165,7 @@ course_axis(rows, course="CS 3100")
 # 
 # **Guide:** https://dash.plotly.com/datatable
 
-# In[58]:
+# In[172]:
 
 
 # Defining columns of interest
@@ -180,7 +180,7 @@ course_components_to_agg = {**TABLE_COMPONENTS, **REVIEW_COMPONENTS}
 course_components_to_agg
 
 
-# In[59]:
+# In[173]:
 
 
 def get_data_for_course_comparison_table(courses: List) -> pd.DataFrame:
@@ -206,7 +206,7 @@ course_data = get_data_for_course_comparison_table(course_tuples)
 course_data
 
 
-# In[60]:
+# In[174]:
 
 
 # making the table in plotly
@@ -260,7 +260,7 @@ table = go.Figure(
 table  # note: I am pretty happy with this. I would like to add more interactivity here.
 
 
-# In[61]:
+# In[175]:
 
 
 # making a source dropdown
@@ -271,7 +271,7 @@ df['name'] = df['mnemonic'] + " " + df['course_number'].astype(str)
 df['name']
 
 
-# In[62]:
+# In[176]:
 
 
 # adding source dropdowns here
@@ -287,20 +287,20 @@ course_mneumoic_dropdown = dcc.Dropdown(
 # 
 # ![Correlating Instructor Reviews](./imgs/correlating_reviews.png)
 
-# In[63]:
+# In[177]:
 
 
 # the goal for this is to create the functions which can control for a  particular factor
 CHECKBOX_SELECTORS = {
     'average': 'GPA',
-    'course_level': 'Course Level (1k, 2k, ...)',
+    # 'course_level': 'Course Level (1k, 2k, ...)',
     'difficulty' : 'Difficulty',
     'hours_per_week': 'Hours Worked Per Week',
     'amount_group': 'Group Work Per Week'
 }
 
 
-# In[64]:
+# In[178]:
 
 
 # making a bunch of checkboxes in plotly
@@ -309,10 +309,11 @@ checklist = dcc.Checklist(
         {'label': v, 'value': k} for k, v in CHECKBOX_SELECTORS.items()
     ],
     value=[],
+    id='features_to_plot'
 )
 
 
-# In[65]:
+# In[179]:
 
 
 # creating a histogram of course reviews
@@ -323,9 +324,74 @@ course_reviews = px.histogram(
 course_reviews
 
 
+# ### Making a regression
+
+# In[180]:
+
+
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# making a linear regression model
+model = LinearRegression()
+
+columns = ['difficulty', 'instructor_rating', 'hours_per_week']
+# getting the data
+new_columns = ['average', 'difficulty', 'hours_per_week', 'amount_group']
+df.dropna(subset=new_columns, inplace=True)
+X = df[new_columns].values.reshape(-1, len(new_columns))
+Y = df['instructor_rating'].values.reshape(-1, 1)
+
+# fitting the model
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+model.fit(X_train, Y_train)
+
+print(model.coef_, model.intercept_)
+
+
+# In[181]:
+
+
+@callback(
+    Output('correlation_plot', 'figure'),
+    Input('features_to_plot', 'value')
+)
+def make_correlation_plot(columns): 
+    """
+    Makes a bar chart of the correlation between columns
+    """
+    # uses the data from the model
+    data = zip(new_columns, model.coef_[0])
+    print(data)
+    # filter the data by which the column is in the columns
+    column_renamer = {
+        'average': 'GPA',
+        'difficulty': 'Difficulty',
+        'hours_per_week': 'Hours Worked Per Week',
+        'amount_group': 'Group Work Per Week'
+    }
+
+    x_labels = []
+    y_values = []
+    for col, r in data:
+        if col in columns:
+            x_labels.append(column_renamer.get(col, col))
+            y_values.append(r)
+    
+    print(x_labels, y_values)
+    
+    fig = px.bar(x=x_labels, y=y_values, labels= {'x': 'Feature', 'y': 'Correlation Coefficient', **column_renamer})
+    return fig
+
+make_correlation_plot(new_columns)
+
+
 # # Final Dashboard Created
 
-# In[66]:
+# In[182]:
 
 
 # defining callbacks
@@ -362,7 +428,10 @@ def update_course_hours(course: str):
     return course_axis(summary, course=course, include="duration")
 
 
-# In[70]:
+### Training a linear model to predict the instructor rating based on (GPA, Course Level, Difficulty, Hours Worked Per Week, Group Work Per Week)
+
+
+# In[ ]:
 
 
 # Making an app to display everything
@@ -396,20 +465,20 @@ course_filter_section = html.Div([
 ### CORRELATING INSTRUCTOR REVIEWS ###
 controlling_factors = html.Div([
     html.H1("Correlating Instructor Reviews"),
-    html.Div("Control for the following factors (not interactive yet)"),
+    html.Div("Control for the following factors"),
     checklist,
 ], className="one-third column")
 
 distribution_of_reviews = html.Div([
     html.H1("Distribution of Residuals"),
     html.P("What's left over?"),
-    dcc.Graph(figure=course_reviews, id="course_reviews"),
+    # dcc.Graph(figure=course_reviews, id="course_reviews"),
 ], className="one-third column")
 
 predictive_power_of_features = html.Div([
     html.H1("Predictive Power of Features"),
     html.P("How well do these features predict the instructor rating?"),
-    # dcc.Graph(figure=course_reviews, id="course_reviews"),
+    dcc.Graph(id="correlation_plot"),
 ], className="one-third column")
 
 correlating_instructor_reviews = html.Div([
@@ -428,5 +497,5 @@ app.layout = html.Div([
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(jupyter_mode='tab')
 
